4f66dcfdb29b1bf2ddd50eccfb36b40ea2dad4a2
Lucas Nogueira Munhoz <ln.munhoz@gmail.com>
2013-06-07 00:43:00 -0300

    Mongo Connection Tests

---
+++ MongoRepositoryTest/MongoRepositoryTest/MongoDb.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoRepository;
+
+
+namespace MongoRepositoryTest
+{
+    public class MyEntity : Entity
+    {
+        public string Name { get; set; }
+    }
+    public class Person : MyEntity
+    {
+
+    }
+
+    public class MongoDb
+    {
+        public static void Main(string[] args)
+        {
+            Insert<Person>("Teste");
+            var entity = GetByName<Person>("Teste");
+        }
+
+        public static T GetByName<T>(string name) where T : MyEntity
+        {
+            var query = Query<T>.EQ(e => e.Name, name);
+            var collection = OpenCollection<T>();
+            var entity = collection.FindOne(query);
+            return entity;
+        }
+
+        public static void Insert<T>(string name) where T : MyEntity, new()
+        {
+            var collection = OpenCollection<T>();
+            collection.Insert(new T { Name = name });
+        }
+
+        public static MongoCollection<T> OpenCollection<T>() where T : MyEntity
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var server = client.GetServer();
+            var database = server.GetDatabase("db");
+            return database.GetCollection<T>(typeof(T).Name);
+        }
+
+    }
+}
